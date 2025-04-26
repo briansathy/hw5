0d6a69fe2e26bbf0d7568248b0d1ed8b43f22af5
Brian Sathy <bsathy@usc.edu>
2025-04-26 04:36:46 +0000

    Implement wordle helper and nurse‐scheduler backtracker

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,35 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+// checks for valid state
 
+bool isValid(
+    int day,
+    Worker_T worker,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const std::vector<int>& count_
+
+);
+
+// does the recursive backtracking
+bool scheduleHelper(
+    int day,
+    int slot,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<int>& count_
+);
 
 // Add your implementation of schedule() and other helper functions here
 
+
+
+// returns true if solution is found
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -38,8 +63,86 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // sets up sched to be nxk matrix of values -1
+    sched.assign(avail.size(), std::vector<Worker_T>(dailyNeed, INVALID_ID));
 
-
+    // create vector of ints to keep track of how many
+    // shifts each worker has been assigned
+    std::vector<int> count(avail[0].size());
+    return scheduleHelper(0, 0, avail, dailyNeed, maxShifts, sched, count);
+}
+bool isValid(
+    int day,
+    Worker_T worker,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const std::vector<int>& count_
+)
+{
+    // checks if worker is available
+    if (avail[day][worker] == 0)
+    {
+        return false;
+    }
+    // if the worker has worked to much
+    else if (count_[worker] >= maxShifts)
+    {
+        return false;
+    }
+    // if the worker is already scheudled that day
+    else if (std::find(sched[day].begin(), sched[day].end(), worker) != sched[day].end())
+    {
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+}
+bool scheduleHelper(
+    int day,
+    int slot,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<int>& count_
+)
+{
+    // # of days and workers
+    size_t n = avail.size();    
+    size_t k = avail[0].size();   
+    // base case
+    if (day == n)
+    {
+        return true;
+    }
+    // if at the end of the day
+    if (slot == dailyNeed)
+    {
+        return scheduleHelper(day + 1, 0, avail, dailyNeed, maxShifts, sched, count_);
+    }
+    // loop throughy all workers
+    for (Worker_T worker = 0; worker < k; worker++)
+    {
+        if (!isValid(day, worker, avail, dailyNeed, maxShifts, sched, count_))
+        {
+            continue;
+        }
+        // insert the worker into slot
+        sched[day][slot] = worker;
+        count_[worker]++;
+        // recursively go to next slot
+        if (scheduleHelper(day, slot + 1, avail, dailyNeed, maxShifts, sched, count_))
+        {
+            return true;
+        }
+        // backtrack
+        sched[day][slot] = INVALID_ID;
+        count_[worker]--;
+    }
+    return false;
 
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -11,9 +11,15 @@
 #include "dict-eng.h"
 using namespace std;
 
-
-// Add prototypes of helper functions here
-
+// Helper function to do the recursion
+void solve(
+    const std::string& origin,
+    std::string& currWord_,
+    std::string floating_,
+    const std::set<std::string>& dict_,
+    std::set<std::string>& words_,
+    int pos
+    );
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,77 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    // Initialize variables so that we can modify them
+    std::string currWord = in;
+    std::set<std::string> words;
+    // Call our helper function to basically do all the work
+    solve(in, currWord, floating, dict, words, 0);
+    return words;
 }
 
 // Define any helper functions here
+// Function that does all the recursion
+void solve(
+    const std::string& origin,
+    std::string& currWord_,
+    std::string floating_,
+    const std::set<std::string>& dict_,
+    std::set<std::string>& words_,
+    int pos
+    )
+{
+    int blanksLeft = 0;
+    for (int i = pos; i < origin.size(); ++i)
+    {
+        if (origin[i] == '-')
+        {
+            ++blanksLeft;
+        }
+    }
+    // impossible case
+    if (floating_.size() > blanksLeft)
+    {
+        return;
+    }
+    // base case
+    if (pos == origin.size())
+    {
+        if (floating_.size() == 0 && dict_.find(currWord_) != dict_.end())
+        {
+        words_.insert(currWord_);
+        }
+        return;
+    }
+    // Check if the letter we are on is a green letter
+    if (origin[pos] != '-')
+    {
+        solve (origin, currWord_, floating_, dict_, words_, pos+1);
+    }
+    // if it is a dash, then loop through each letter in the alphabet to add
+    // and recurse
+    else
+    {
+        // looping through all floating characters
+        for (int i = floating_.size() - 1; i >= 0 ; --i)
+        {
+            char c = floating_[i];
+            currWord_[pos] = c;
+            std::string newFloat = floating_;
+            newFloat.erase(i, 1);
+            solve(origin, currWord_, newFloat, dict_, words_, pos+1);
+        }
+        // looping through all other alphabetical options
+        if (floating_.size() < blanksLeft)
+        {
+            for (int i = 97; i < 123; ++i)
+            {
+                if (floating_.find(i) == std::string::npos)
+                {
+                    currWord_[pos] = i;
+                    solve(origin, currWord_, floating_, dict_, words_, pos+1);
+                }
+            }
+        }
+
+    }
+}
